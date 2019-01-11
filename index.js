ef1ab4e1a3f049c546b02dd3a2f700af808157fa
Tammy Metz <tammy@tammymetz.com>
2019-01-11 03:31:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function switchOnCharmFromTip(tip) {
   if (tip == "generous") {
     return "Thank you so much.";
   }
-  else if (tip == "not so generous"){
+  else if (tip == "not as generous"){
     return "Thank you.";
   }
   else {
