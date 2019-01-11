9c5661f38f2bcb4ecee304c2df94b854dafa075b
Tammy Metz <tammy@tammymetz.com>
2019-01-11 03:32:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500) {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city == "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  if (tip == "generous") {
+    return "Thank you so much.";
+  }
+  else if (tip == "not as generous"){
+    return "Thank you.";
+  }
+  else {
+    return "Bye.";
+  }
+}
