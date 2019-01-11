5eda2806147ea3e4cfc2aec6267c0515c4664e6a
Tammy Metz <tammy@tammymetz.com>
2019-01-11 03:29:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function scuberGreetingForFeet(distance) {
     return "No can do.";
   }
 }
+
+function ternaryCheckCity(city) {
+  if (city == "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
+}
